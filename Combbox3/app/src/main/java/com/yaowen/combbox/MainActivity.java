4302942644b9c7e265bc0261f646aa85994bb8a5
茗香墨语 <yaowen424@126.com>
2015-10-07 15:18:37 +0800

    修改master分支代码，实现了简单的ArrayAdapter适配器来刷新combbox控件item的内容；

--- Combbox3/app/src/main/java/com/yaowen/combbox/MainActivity.java
+++ Combbox3/app/src/main/java/com/yaowen/combbox/MainActivity.java
@@ -1,15 +1,11 @@
 package com.yaowen.combbox;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,6 +14,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Combbox combbox;
     private List<String> items;
+    private ArrayAdapter adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +22,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
         combbox = (Combbox) findViewById(R.id.combbox);
         getData();
-        // combbox.setAdapter(adapter);
-        combbox.setAdapter(new MyAdapter(this));
+        adapter = new ArrayAdapter(this, R.layout.item_layout, R.id.tv, items);
+        combbox.setAdapter(adapter);
         combbox.setOnItemSelectedListener(this);
     }
 
@@ -48,42 +45,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    class MyAdapter extends BaseAdapter {
-        private LayoutInflater mLayoutInflater;
-        private Context context = null;
-
-        public MyAdapter(Context context) {
-            this.context = context;
-            mLayoutInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final TextView indexText;
-            if (convertView == null) {
-                convertView = mLayoutInflater.inflate(R.layout.item_layout, null);
-            }
-            indexText = (TextView) convertView.findViewById(R.id.tv);
-            // 设置item中indexText的文本
-            indexText.setText(items.get(position).toString());
-            return convertView;
-        }
     }
 }
